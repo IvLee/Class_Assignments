eb4d456f1566bf0673b0fba5fc1890b6339d42f9
Ivan <s@gmail.com>
2017-02-11 15:46:14 -0500

    CLASSWORK

---
+++ Prime numbers.py
@@ -0,0 +1,15 @@
+def prime(n):
+    i = 2
+    while i < n :
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+i = 2
+while i <= 100 :
+    if prime(i):
+        print(i, "is a Prime Number")
+    i= i+1
+
+print("end")
