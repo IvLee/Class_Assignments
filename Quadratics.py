994ef6c5678a11f28a4a36e53ad2ec28b3350181
Ivan <ilee.4123@gmail.com>
2017-02-10 23:27:48 -0500

    Added some more stuff... learned some new things ^_^

--- Quadratics.py
+++ Quadratics.py
@@ -7,13 +7,34 @@ time.sleep(2)
 def  quadratic ():
     print("Please input values for the quadratic formula : Ax\u00b2+Bx+C")
     time.sleep(2)
-    a = int(input("What is your value of A? "))
-    b = int(input("What is your value of B? "))
-    c = int(input("What is your value of C? "))
-    d = int(input("What is your value of X? "))
-    print('{}({:-})^2{:+}({:-}){:+} = '.format(a,d,b,d,c), ((a*(d**2))+(b*d)+c))
-    time.sleep(2)
-    ask()
+    try :
+        a = int(input("What is your value of A? "))
+    except ValueError :
+        time.sleep(1)
+        print("Come on, this is math not English. Give me a number!")
+        quadratic()
+    try :
+        b = int(input("What is your value of B? "))
+    except ValueError :
+        time.sleep(1)
+        print("Made a mistake? Try again...")
+        quadratic()
+    try :
+        c = int(input("What is your value of C? "))
+    except ValueError :
+        time.sleep(2)
+        print("Please be serious about this.. you were almost there..")
+        quadratic()
+    try :
+        d = int(input("What is your value of X? "))
+    except ValueError :
+        time.sleep(4)
+        print("You just had to mess this up, huh!? You know what? NO MATH FOR YOU!")
+        exit()
+    else :
+        print('{}({:-})^2{:+}({:-}){:+} = '.format(a,d,b,d,c), ((a*(d**2))+(b*d)+c))
+        time.sleep(2)
+        ask()
 
 def ask () :
     answer = input("Want to do another one? ")
