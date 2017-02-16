9e1e0fa2b670e00aab4ded00471b8a79e576f4cc
Ivan <ilee.4123@gmail.com>
2017-02-15 19:33:21 -0500

    Max and min

--- Maximum and Minimum.py
+++ Maximum and Minimum.py
@@ -1,25 +1,35 @@
 def main() :
     array = list()
-    try:
-        average = int(input("How many elements do you want for the array? "))
-    except:
-        print('Invalid input, please input a number: ')
-        main()
 
     def elements():
-        for i in range (average) :
-            try:
+        try:
+            average = int(input("How many elements do you want for the array? "))
+            for i in range(average):
                 num = int(input("Enter the numbers for the array: "))
                 array.append(num)
-            except ValueError:
-                print('Invalid input, please input a number: ')
-                elements()
-        print(array)
+            print(array)
+        except:
+            print('Invalid input, start again. ')
+            elements()
+
+
+    def max():
+        mx = 0
+        for i in range(len(array)):
+            if array[i] > mx:
+                mx = array[i]
+        print('Max: {}'.format(mx))
+
+    def min():
+        mn = array[0]
+        for i in range(len(array)):
+            if array[i] <= mn:
+                mn = array[i]
+        print('Min: {}'.format(mn))
+
     elements()
-
-    for i in range(len(array)):
-
-
+    max()
+    min()
 
 main()
 
